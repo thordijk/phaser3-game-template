d0bb8bbfa0004f14cf4d7840184c94752a9587c6
sven van straalen <sven.van.straalen@capgemini.com>
2021-07-23 23:18:29 +0200

    added linter to make it pritty

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
 // eslint-disable-next-line
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "ignorePatterns": [".eslint.js", "webpack.config.js"],
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "rules": {
-    }
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  ignorePatterns: ['.eslint.js', 'webpack.config.js'],
+  extends: ['airbnb', 'airbnb/whitespace'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+  },
 };
--- src/main.js
+++ src/main.js
@@ -1,39 +1,31 @@
 import Phaser from 'phaser';
-import logoImg from './assets//img/player.png';
+import logoImg from './assets/img/player.png';
 
-class MyGame extends Phaser.Scene
-{
-    constructor ()
-    {
-        super();
-    }
+class MyGame extends Phaser.Scene {
+  preload() {
+    this.load.image('logo', logoImg);
+  }
 
-    preload ()
-    {
-        this.load.image('logo', logoImg);
-    }
-      
-    create ()
-    {
-        const logo = this.add.image(400, 150, 'logo');
-      
-        this.tweens.add({
-            targets: logo,
-            y: 450,
-            duration: 2000,
-            ease: "Power2",
-            yoyo: true,
-            loop: -1
-        });
-    }
+  create() {
+    const logo = this.add.image(400, 150, 'logo');
+
+    this.tweens.add({
+      targets: logo,
+      y: 450,
+      duration: 2000,
+      ease: 'Power2',
+      yoyo: true,
+      loop: -1,
+    });
+  }
 }
 
 const config = {
-    type: Phaser.AUTO,
-    parent: 'phaser-example',
-    width: 800,
-    height: 600,
-    scene: MyGame
+  type: Phaser.AUTO,
+  parent: 'phaser-example',
+  width: 800,
+  height: 600,
+  scene: MyGame,
 };
 
 // eslint-disable-next-line
